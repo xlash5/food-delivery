2ccf58fc566289de1b1c71977ed5441c46d28ba9
Enes <menesilhan@gmail.com>
2020-08-20 20:44:52 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'screens/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Food Delivery UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.grey[50],
+        primaryColor: Color(0xff3d6cb9),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
